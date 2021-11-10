71862977e1e8ab8603b6dd23932601566c580766
SSovile <vladyslav.tykhanskyi.knm.2020@lpnu.ua>
2021-11-10 18:40:28 +0200

    Lab4 update

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
-## Task 3
-Farmer John has built a new long barn, with N (2 <= N <= 100,000) stalls. The stalls are located along a straight line at positions x1,...,xN (0 <= xi <= 1,000,000,000).
+## Task 4
 
-His C (2 <= C <= N) cows don't like this barn layout and become aggressive towards each other once put into a stall. 
-To prevent the cows from hurting each other, FJ wants to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible. 
-What is the largest minimum distance?
+Dijkstra's Algorithm
 
-
-## How to run 
-Clone repo and run cpp file
+Create algorithm which found the shortest path
--- main.py
+++ main.py
@@ -1,58 +1,51 @@
 from queue import PriorityQueue
 
-class Graph:
+class Djikstra_Graph:
 
-    def __init__(self, num_of_vertices):
-        self.v = num_of_vertices  
-        self.edges = [[-1 for i in range(num_of_vertices)] for j in range(num_of_vertices)]
+    def __init__(self, amount):
+        self.g = amount
+        self.edges = [[-1 for i in range(amount)] for j in range(amount)]
         self.visited = []
 
-    def add_edge(self, u, v, weight):
-        self.edges[u][v] = weight
-        self.edges[v][u] = weight
+    def djikstra(self, start):
+        G = {g: float('inf') for g in range(self.g)}
 
-    def dijkstra(self, start_vertex):
-        D = {v: float('inf') for v in range(self.v)}   
-        D[start_vertex] = 0
+        G[start] = 0
 
-        pq = PriorityQueue()
-        pq.put((0, start_vertex))
+        queq = PriorityQueue()
+        queq.put((0, start))
 
-        while not pq.empty():
-            (dist, current_vertex) = pq.get()
-            self.visited.append(current_vertex)
+        while not queq.empty():
+            (dist, current) = queq.get()
+            self.visited.append(current)
 
-            for neighbor in range(self.v):
-                if self.edges[current_vertex][neighbor] != -1:
-                    distance = self.edges[current_vertex][neighbor]
+            for neighbor in range(self.g):
+                if self.edges[current][neighbor] != -1:
+                    distance = self.edges[current][neighbor]
                     if neighbor not in self.visited:
-                        old_cost = D[neighbor]
-                        new_cost = D[current_vertex] + distance
-                        if new_cost < old_cost:
-                            pq.put((new_cost, neighbor))
-                            D[neighbor] = new_cost
-        return D
+                        old = G[neighbor]
+                        new = G[current] + distance
+                        if new < old:
+                            queq.put((new, neighbor))
+                            G[neighbor] = new
+        return G
 
+
+    def add_edge(self, u, g, weight):
+        self.edges[u][g] = weight
+        self.edges[g][u] = weight
 
 if __name__ == '__main__':
-    g = Graph(9)
-    g.add_edge(0, 1, 4)
-    g.add_edge(0, 6, 7)
-    g.add_edge(1, 6, 11)
-    g.add_edge(1, 7, 20)
-    g.add_edge(1, 2, 9)
-    g.add_edge(2, 3, 6)
-    g.add_edge(2, 4, 2)
-    g.add_edge(3, 4, 10)
-    g.add_edge(3, 5, 5)
-    g.add_edge(4, 5, 15)
-    g.add_edge(4, 7, 1)
-    g.add_edge(4, 8, 5)
-    g.add_edge(5, 8, 12)
-    g.add_edge(6, 7, 1)
-    g.add_edge(7, 8, 3)
 
-    D = g.dijkstra(0)
+    el = Djikstra_Graph(5)
+    el.add_edge(0, 1, 2)
+    el.add_edge(0, 3, 1)
+    el.add_edge(1, 2, 8)
+    el.add_edge(1, 3, 3)
+    el.add_edge(1, 4, 10)
+    el.add_edge(2, 4, 9)
 
-    for vertex in range(len(D)):
-        print("Distance from vertex 0 to vertex", vertex, "is", D[vertex])
+    G = el.djikstra(0)
+
+    for vertex in range(len(G)):
+        print("Distance from vertex 0 to vertex", vertex, "is", G[vertex])
