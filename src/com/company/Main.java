de73fff67ed7377b872e5d65282ccad983139062
SSovile <vladyslav.tykhanskyi.knm.2020@lpnu.ua>
2021-12-06 17:08:36 +0200

    Lab6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
-
 public class Main {
-
-
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException{
         List<String> whain = new ArrayList<>();
-        List<String> whaout = new ArrayList<>();
         String path = "C:\\Users\\vlada\\IdeaProjects\\wchain\\src\\com\\company\\logs";
         File file = new File(path);
         Scanner scannerfile = new Scanner(file);
@@ -21,56 +17,13 @@ public class Main {
             whain.add(scannerfile.nextLine());
         }
 
-        Comparator<String> stringLengthComparator = (o1, o2) -> Integer.compare(o2.length(), o1.length());
-
-        whain.sort(stringLengthComparator);
-
-        System.out.println(whain);
-        int counter = 0 ;
-
-        //       int start = scanner.nextInt();
-        //       System.out.println(start);
-
-//Work:2
-//        for (String s : whain) {
-//            if (whain.get(start).contains(s)) {
-//                wha.add(s);
-//            }
-//        }
-//
-//        for(int i = 0; i< wha.size();i++){
-//            if(wha.get(0).length() - wha.get(i).length() == i){
-//                whaout.add(wha.get(i));
-//            }
-//        }
-
-        String start = whain.get(0);
-        int k = 0;
-        String temp = whain.get(k);
-
-        for(int i = 0; i< whain.size();i++){
-            if(start.contains(temp)){
-                whaout.add(temp);
-                start = whain.get(k);
-                if(i == whain.size()-1){
-                    break;
-                }
-            }
-                temp = whain.get(++k);
-        }
-
-        for (int i = 0;i<whaout.size();i++) {
-            if(whaout.get(0).length() - whaout.get(i).length() == i){
-                counter++;
-                System.out.println(whaout.get(i));
-            }
-        }
-        System.out.println(counter);
+        String[] s = whain.toArray(new String[0]);
+        Whain sol = new Whain();
 
         try {
             FileWriter myWriter = new FileWriter("wchaout.txt");
 
-            myWriter.write("Counter: "+counter);
+            myWriter.write("Counter: "+ sol.longestStrChain(s));
 
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
