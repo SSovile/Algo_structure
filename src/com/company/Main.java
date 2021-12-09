9fcdd21ea900267791734aa2c1cbf71aa29d1faa
SSovile <vladyslav.tykhanskyi.knm.2020@lpnu.ua>
2021-12-09 19:46:04 +0200

    lab7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,36 +1,14 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String txt = "ABCDE FTDE";
+        String pat = "DE";
 
-public class Main {
-    public static void main(String[] args) throws FileNotFoundException{
-        List<String> whain = new ArrayList<>();
-        String path = "C:\\Users\\vlada\\IdeaProjects\\wchain\\src\\com\\company\\logs";
-        File file = new File(path);
-        Scanner scannerfile = new Scanner(file);
+        int q = 1;
 
-        while (scannerfile.hasNextLine()) {
-            whain.add(scannerfile.nextLine());
-        }
-
-        String[] s = whain.toArray(new String[0]);
-        Whain sol = new Whain();
-
-        try {
-            FileWriter myWriter = new FileWriter("wchaout.txt");
-
-            myWriter.write("Counter: "+ sol.longestStrChain(s));
-
-            myWriter.close();
-            System.out.println("Successfully wrote to the file.");
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-
+        RabinKarp.search(pat, txt, q);
     }
 }
---
+++ src/com/company/Test.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Test {
+    String txt = "ABCDE FTDE";
+    String pat = "DE";
+
+}
